47ee701cd1f89194ddf4b4ba44d1d68245207dbe
Ursule Phrinfo <ufrinfo@eduroam-prg-sg-1-47-159.net.univ-paris-diderot.fr>
2022-01-26 10:00:38 +0100

    <commit message>

---
+++ space-war/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  space-war
+//
+//  Created by Ursule Phrinfo on 26/01/2022.
+//  Copyright © 2022 PIERGBE. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
